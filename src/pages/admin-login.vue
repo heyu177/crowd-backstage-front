5273c9b443402b34cd5dd03d63474a0fc95cf801
heyu <heyu@qq.com>
2020-07-10 22:29:26 +0800

    2020.7.10 22:29已提交

--- src/pages/admin-login.vue
+++ src/pages/admin-login.vue
@@ -6,21 +6,41 @@
                   <a href="#" class="title">尚筹网-创意产品众筹平台</a>
               </h1>
           </el-header>
-          <el-main></el-main>
+          <el-main>
+              <el-form>
+                  <el-form-item>
+                      <h1>用户登陆</h1>
+                  </el-form-item>
+                  <el-form-item>
+                      <el-input placeholder="请输入登陆账号" v-model="account"></el-input>
+                  </el-form-item>
+                  <el-form-item>
+                      <el-input placeholder="请输入登陆密码" show-password v-model="pwd"></el-input>
+                  </el-form-item>
+              </el-form>
+          </el-main>
       </el-container>
   </div>
 </template>
 
 <script>
 import Vue from "vue"
-import {Container,Header,Main} from "element-ui"
+import {Container,Header,Main,Form,Input,FormItem} from "element-ui"
 
 Vue.use(Container)
 Vue.use(Header)
 Vue.use(Main)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(FormItem)
 
 export default {
-
+    data(){
+        return {
+            account:"",
+            pwd:""
+        }
+    }
 }
 </script>
 
@@ -43,5 +63,17 @@ export default {
 }
 .el-header h1 .title:hover{
     color: #fff;
+}
+.el-form{
+    text-align: center;
+}
+.el-form-item h1{
+    width: 500px;
+    margin: 0 auto;
+    text-align: left;
+    font-size: 30px;
+}
+.el-input{
+    width: 500px;
 }
 </style>

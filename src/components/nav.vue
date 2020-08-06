4dc057b4083856a433b1c899071370f9312b4e3d
heyu <508540105@qq.com>
2020-08-06 23:10:01 +0800

    2020.8.6 23:09已提交

---
+++ src/components/nav.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-collapse>
+    <el-collapse-item>
+      <template slot="title">
+        <h3>
+          <i class="el-icon-aim"></i>控制面板
+        </h3>
+      </template>
+    </el-collapse-item>
+    <el-collapse-item>
+      <template slot="title">
+        <h3>
+          <i class="el-icon-s-unfold"></i>权限管理 <el-badge :value="3" class="mark"></el-badge>
+        </h3>
+      </template>
+      <el-menu>
+        <el-menu-item index="1">
+          <i class="el-icon-user-solid"></i>用户维护
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-s-check"></i>角色维护
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-s-goods"></i>菜单维护
+        </el-menu-item>
+      </el-menu>
+    </el-collapse-item>
+  </el-collapse>
+</template>
+
+<script>
+import Vue from "vue";
+import { Collapse, Menu,Badge} from "element-ui";
+
+Vue.use(Collapse);
+Vue.use(Menu);
+Vue.use(Badge)
+
+export default {};
+</script>
+
+<style lang="less" scoped>
+.el-collapse-item {
+  h3 {
+    font-size: 15px;
+    font-weight: normal;
+  }
+  &:nth-child(1) {
+    h3 {
+      color: #338acc;
+    }
+  }
+}
+</style>>
--- src/pages/admin-main.vue
+++ src/pages/admin-main.vue
@@ -3,75 +3,105 @@
     <el-header>
       <h1>众筹平台 - 控制面板</h1>
       <div class="button">
-        <el-button type="danger"><i class="el-icon-question"></i>帮助</el-button>
+        <el-button type="danger">
+          <i class="el-icon-question"></i>帮助
+        </el-button>
       </div>
       <el-dropdown>
         <el-button type="success">
-          <i class="el-icon-user-solid"></i>张三<i class="el-icon-caret-bottom"></i>
+          <i class="el-icon-user-solid"></i>张三
+          <i class="el-icon-caret-bottom"></i>
         </el-button>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item><i class="el-icon-s-tools"></i>个人设置</el-dropdown-item>
-          <el-dropdown-item><i class="el-icon-chat-dot-square"></i>消息</el-dropdown-item>
-          <el-dropdown-item divided><i class="el-icon-switch-button"></i>退出系统</el-dropdown-item>
+          <el-dropdown-item>
+            <i class="el-icon-s-tools"></i>个人设置
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <i class="el-icon-chat-dot-square"></i>消息
+          </el-dropdown-item>
+          <el-dropdown-item divided>
+            <i class="el-icon-switch-button"></i>退出系统
+          </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
       <el-input placeholder="查询" v-model="search"></el-input>
     </el-header>
     <el-container>
-      <el-aside>bbb</el-aside>
+      <el-aside>
+        <Nav />
+      </el-aside>
       <el-main>ccc</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-import Vue from "vue"
-import {Container,Input,Dropdown,Button} from "element-ui"
+import Vue from "vue";
+import { Container, Input, Dropdown, Button } from "element-ui";
+import Nav from "../components/nav.vue";
 
-Vue.use(Container)
-Vue.use(Input)
-Vue.use(Dropdown)
-Vue.use(Button)
+Vue.use(Container);
+Vue.use(Input);
+Vue.use(Dropdown);
+Vue.use(Button);
 
 export default {
-  data(){
+  data() {
     return {
-      search:""
-    }
-  }
-}
+      search: "",
+    };
+  },
+  components: {
+    Nav,
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.el-header{
-  background: #222;
-  h1{
-    color: #9d9d9d;
-    margin:auto 0;
-    height: 100%;
-    line-height: 60px;
-    font-size: 32px;
-    font-weight: normal;
-    float: left;
+.el-container {
+  .el-header {
+    background: #222;
+    h1 {
+      color: #9d9d9d;
+      margin: auto 0;
+      height: 100%;
+      line-height: 60px;
+      font-size: 32px;
+      font-weight: normal;
+      float: left;
+      &:hover {
+        color: #fff;
+      }
+    }
+    .el-input {
+      float: right;
+      width: 200px;
+      height: 100%;
+      line-height: 60px;
+      margin: 0 5px;
+    }
+    .el-dropdown {
+      float: right;
+      margin: auto 5px;
+      height: 100%;
+      line-height: 60px;
+    }
+    .button {
+      float: right;
+      margin: auto 5px;
+      height: 100%;
+      line-height: 60px;
+    }
   }
-  .el-input{
-    float: right;
-    width: 200px;
-    height: 100%;
-    line-height: 60px;
-    margin: 0 5px;
-  }
-  .el-dropdown{
-    float: right;
-    margin: auto 5px;
-    height: 100%;
-    line-height: 60px;
-  }
-  .button{
-    float: right;
-    margin: auto 5px;
-    height: 100%;
-    line-height: 60px;
+  .el-container {
+    .el-aside {
+      background: #f5f5f5;
+      padding: 12px;
+      position: absolute;
+      left: 0;
+      top: 60px;
+      bottom: 0;
+    }
   }
 }
 </style>>
